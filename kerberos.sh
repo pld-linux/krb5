8af2252d62464220c2d5882f5b580cd625d0bf64
wojtek <wojtek@pld.org.pl>
1999-06-12 12:02:30 +0000

    /etc/profile.d
    Changed files:
        kerberos.csh -> 1.1
        kerberos.sh -> 1.1

---
+++ kerberos.sh
@@ -0,0 +1,7 @@
+alias	kinit="kinit -l 10m"
+alias	klist="klist -efc"
+alias	kdestroy="kdestroy -q"
+alias	krcp="/usr/athena/bin/rcp -x"
+alias	krlogin="/usr/athena/bin/rlogin -x"
+alias	krsh="/usr/athena/bin/rsh -x"
+alias	ktelnet="/usr/athena/bin/telnet -x"
